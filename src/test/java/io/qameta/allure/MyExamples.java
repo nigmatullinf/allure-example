2e289d594f993898e5fa0209c1930ff9c7c4686a
Tatiana Ignateva <itz89@YANDEX.RU>
2024-09-25 17:53:34 +0300

    Update MyExamples.java

--- src/test/java/io/qameta/allure/MyExamples.java
+++ src/test/java/io/qameta/allure/MyExamples.java
@@ -16,7 +16,7 @@ import static io.qameta.allure.Allure.step;
      */
 
     @Test
-    @AllureId("183369")
+    @AllureId("9251")
     @DisplayName("Some test")
         @Description(useJavaDoc = true)
         public void test1667306661111() {
